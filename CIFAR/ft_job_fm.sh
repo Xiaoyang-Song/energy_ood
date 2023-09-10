e9a3a2cc0ac5efcbd9b5e16762f135138037a285
xysong <xysong25@gmail.com>
2023-09-10 06:27:32 -0400

    Update: imbalanced regime ft

--- CIFAR/ft_job_fm.sh
+++ CIFAR/ft_job_fm.sh
@@ -9,9 +9,9 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-gpu=12GB
 #SBATCH --time=24:00:00
-#SBATCH --output=/scratch/sunwbgt_root/sunwbgt98/xysong/jobs/Energy-FT-FM.log
+#SBATCH --output=/scratch/sunwbgt_root/sunwbgt98/xysong/jobs/Energy-FT-FM-II.log
 
 for N in 4 8 16 32 64 128 256 512 1024 2048 4096; do
-    python train.py --dataset FashionMNIST --score energy --n_ood $N --regime Balanced --epochs 100
-    python test.py --dataset FashionMNIST --score energy --num_to_avg 5 --type ft --ft_n $N --ft_epochs 99 --regime Balanced
+    python train.py --dataset FashionMNIST --score energy --n_ood $N --regime Imbalanced --epochs 100
+    python test.py --dataset FashionMNIST --score energy --num_to_avg 5 --type ft --ft_n $N --ft_epochs 99 --regime Imbalanced
 done
--- CIFAR/schedule.sh
+++ CIFAR/schedule.sh
@@ -2,8 +2,8 @@
 # Environment Configuration
 export PYTHONPATH=$PYTHONPATH$:`pwd`
 
-sbatch ft_job_cs.sh
+# sbatch ft_job_cs.sh
 sbatch ft_job_sv.sh
-sbatch ft_job_m.sh
+# sbatch ft_job_m.sh
 sbatch ft_job_fm.sh
-sbatch ft_job_mfm.sh
+# sbatch ft_job_mfm.sh
