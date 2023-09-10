d901ce9e470db201db2ecb778499900d981a273c
xysong <xysong25@gmail.com>
2023-09-10 00:48:52 -0400

    Update: scheduler scripts

---
+++ CIFAR/schedule.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Environment Configuration
+export PYTHONPATH=$PYTHONPATH$:`pwd`
+
+sbatch ft_job_cs.sh
+sbatch ft_job_sv.sh
+sbatch ft_job_m.sh
+sbatch ft_job_fm.sh
+sbatch ft_job_mfm.sh
